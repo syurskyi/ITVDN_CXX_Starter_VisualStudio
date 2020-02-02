368cf772075b6a8abe5812550e4129335e978c2e
Grey <sergejyurskyj@yahoo.com>
2020-02-01 21:17:50 -0800

    Committed 004_Conditionals 002_If_else

---
+++ 005_Logic/001_Logic/005_Logic/001_and/001_and.cpp
@@ -0,0 +1,45 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+/*
+	Конъюнкция – это логическая операция, ставящая в соответствие каждым двум простым высказываниям составное высказывание, 
+	являющееся истинным только тогда, когда оба исходных высказывания истинны. Операция конъюнкции по своему применению
+	близка к союзу «И». 
+
+					-------------------------
+					|	a	|	b	|	&&	|
+					-------------------------
+					| true	| true	| true	|
+					-------------------------
+					| true	| false	| false	|
+					-------------------------
+					| false	| true	| false	|
+					-------------------------
+					| false	| false	| false	|
+					-------------------------
+*/
+
+int main()
+{
+	bool a = true;		// 1
+	bool b = true;		// 0
+
+	bool result = a && b;
+
+	cout << boolalpha << result << endl;
+
+    return 0;
+}
+
+//#include "stdafx.h"
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//
+//
+//	return 0;
+//}
