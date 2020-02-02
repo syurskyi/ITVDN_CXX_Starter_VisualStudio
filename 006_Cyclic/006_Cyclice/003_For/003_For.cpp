268da82f81ab5203e7d91ed03086a6a0b75fd972
Grey <sergejyurskyj@yahoo.com>
2020-02-01 21:20:51 -0800

    Committed 006_Cyclic 003_For

---
+++ 006_Cyclic/006_Cyclice/003_For/003_For.cpp
@@ -0,0 +1,23 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int count = 5;
+
+	for (int i = 0; i < count; i++)
+	{
+		cout << "Iteration " << i << endl;
+
+		break;	// Эта  инстукция остановит работу цикла, и предаст управление инструкцие находящейся после операторных скобок цикла 
+
+		cout << "This line is never executed";
+	}
+
+	cout << "This line is always executed";
+
+	cin.get();	
+	return 0;
+}
