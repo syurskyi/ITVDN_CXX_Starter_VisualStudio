86fb72111319b97c6cbb1488d3222fbadff33b2b
Grey <sergejyurskyj@yahoo.com>
2020-02-01 21:21:17 -0800

    Committed 006_Cyclic 004_For

---
+++ 006_Cyclic/006_Cyclice/004_For/004_For.cpp
@@ -0,0 +1,23 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int count = 5;
+
+	for (int i = 0; i < count; i++)
+	{
+		cout << "Iteration " << i << endl;
+		
+		continue;	// Эта  инстукция прекратит выполнение текущей итерации, и передаст управление секции изменения счетчика
+
+		cout << "This line is never executed";
+	}
+
+	cout << "This line is always executed";
+
+	cin.get();
+	return 0;
+}
